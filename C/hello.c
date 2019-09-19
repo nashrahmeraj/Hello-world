fb1469a097cda5d201eeba20819af0cf6a442b40
root <root@ip-172-31-6-135.us-east-2.compute.internal>
2019-09-19 10:01:14 +0000

    comit

--- C/hello.c
+++ C/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>   //header file
 int main()           //main function 
 {
-  printf("Hello World ! \n:)");     //print statement
-  return 0;
+  printf("HHello World ! \n:)");     //print statement
+  return 1;
 }
