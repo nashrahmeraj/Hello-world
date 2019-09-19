b28c2a7f220a56046be27cbc9a389dfeabfb617b
nashrahmeraj <55125571+nashrahmeraj@users.noreply.github.com>
2019-09-19 15:42:15 +0530

    Update hello.c

--- C/hello.c
+++ C/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>   //header file
 int main()           //main function 
 {
-  printf("This text has been changed ! \n:)");     //print statement
+  printf("This text has been changed again ! \n:)");     //print statement
   return 1;
 }
