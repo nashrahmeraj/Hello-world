fcb77eb51b26d63784d58f5d4ec1df4aa191930d
root <root@ip-172-31-6-135.us-east-2.compute.internal>
2019-09-19 10:08:03 +0000

    changed basic

--- C/hello.c
+++ C/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>   //header file
 int main()           //main function 
 {
-  printf("HHello World ! \n:)");     //print statement
+  printf("This text has been changed ! \n:)");     //print statement
   return 1;
 }
